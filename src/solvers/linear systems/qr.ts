f174542702d737e099299cf907f78acbb9786ed5
Musseffect <lazarevich356@gmail.com>
2023-10-23 22:43:12 +0700

    Changes

--- src/solvers/linear systems/qr.ts
+++ src/solvers/linear systems/qr.ts
@@ -2,20 +2,32 @@
 
 import Matrix from "../../denseMatrix";
 import { assert } from "../../utils";
-import Vector from "../../vector";
-import { ConvergenseFailureException } from "./exceptions";
 
 const SolverName = "QR";
 
-class QR {
-    A: Matrix;
-    Q: Matrix;
-    R: Matrix;
-    protected decompose() {
+export default class QR {
+    A: Matrix = null;
+    q: Matrix;
+    r: Matrix;
+    _makeCompact: boolean;
+    public factorize(A: Matrix | null) {
+        this.q = null;
+        this.r = null;
+        if (A == null) return;
         assert(this.A.isSquare(), "Expected square matrix");
         throw new Error("Not implemented");
     }
-    constructor(A: Matrix, compact: boolean) {
-        this.decompose();
+    constructor(A: Matrix | null, makeCompact: boolean = false) {
+        this._makeCompact = makeCompact;
+        this.factorize(A);
+    }
+    set makeCompact(value: boolean) {
+        this._makeCompact = value;
+    }
+    get Q(): Matrix {
+        return this.q;
+    }
+    get R(): Matrix {
+        return this.r;
     }
 }
